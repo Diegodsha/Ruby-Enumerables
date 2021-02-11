be9f90640a5e35a87928f29a2c8d06cc141ad788
Morenikeji Bello <caeserselassie@gmail.com>
2021-02-11 18:17:53 +0100

    Write logic for my_each_with_index

--- enumerable_module.rb
+++ enumerable_module.rb
@@ -4,6 +4,23 @@ module Enumerable
       yield to_a[i]
     end
   end
+
+  def my_each_with_index
+    length.times do |i|
+      yield to_a[i], i
+    end
+  end
 end
 
 [1, 2, 3, 4, 5].my_each { |num| puts num }
+
+puts
+
+20.times { print '-' }
+puts
+puts 'THIS IS MY_EACH_WITH_INDEX_EXECUTION'
+
+my_array = { name: 'Diego', country: 'Mexico' }
+my_array.my_each_with_index do |data, idx|
+  puts "The #{data[0]} is #{data[1]}" if idx == 1
+end
