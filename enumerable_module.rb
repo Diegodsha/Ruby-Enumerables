7c338a92c77a617a9f443a49ef9b00f25176f80f
Diego <diegodsha@hotmail.com>
2021-02-11 13:39:02 -0600

    Write my_select logic

--- enumerable_module.rb
+++ enumerable_module.rb
@@ -10,6 +10,15 @@ module Enumerable
       yield to_a[i], i
     end
   end
+
+  def my_select
+    selected = []
+    length.times do |i|
+      condition = yield to_a[i]
+      selected.push(to_a[i]) if condition
+    end
+    selected
+  end
 end
 
 [1, 2, 3, 4, 5].my_each { |num| puts num }
@@ -24,3 +33,9 @@ my_array = { name: 'Diego', country: 'Mexico' }
 my_array.my_each_with_index do |data, idx|
   puts "The #{data[0]} is #{data[1]}" if idx == 1
 end
+
+20.times { print '-' }
+puts
+puts 'THIS IS MY_SELECT'
+
+[2, 3, 4, 6, 7, 16].my_select { |num| puts num if num.even? }
