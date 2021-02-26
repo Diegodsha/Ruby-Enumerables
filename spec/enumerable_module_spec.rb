a1bc0af2e4207f7811f75b5952476a48c9e25853
EroAuditore <alfa.fenrir@gmail.com>
2021-02-26 15:37:37 -0600

    Test case: #my_each_with_index

--- spec/enumerable_module_spec.rb
+++ spec/enumerable_module_spec.rb
@@ -8,7 +8,7 @@ describe Enumerable do
     let(:array) { Array.new(ARRAY_SIZE) {rand(LOWEST_VALUE...HIGHEST_VALUE)}} 
     let(:array_clone) { array.clone } 
     let(:block) { proc {|value| value + 1} } 
-
+    let(:block_index) { proc {|value, index| value + index} } 
 describe "#my_each" do
     it "Returns an Enumerator if no block is given" do
         expect(array.my_each).to be_an Enumerator
@@ -26,6 +26,26 @@ describe "#my_each" do
         my_output = 0
         array.each(&block)
         expect(first_output).to eq(my_output)
+    end
+end
+
+describe "#my_each_with_index" do
+    it "Returns an Enumerator if no block is given" do
+        expect(array.my_each_with_index).to be_an Enumerator
+    end
+
+    it "It does not mutate the original array" do
+        expect(array.my_each_with_index(&block)).to eq(array_clone)
+    end
+    
+    it "It excecutes the given code block once for each element and takes the index of each element" do
+        index_sum = 0
+        block = proc {|value, index| index_sum += index}
+        array.my_each_with_index(&block)
+        first_index_sum = index_sum.dup
+        index_sum = 0
+        array.each_with_index(&block)
+        expect(first_index_sum).to eq(index_sum)
     end
 end
 
